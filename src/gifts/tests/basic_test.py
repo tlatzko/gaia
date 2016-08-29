11feda7b8505d7e42ad09fec496b3fb92c801c53
Thomas Latzko <thomas.latzko@kit.edu>
2016-08-29 13:58:55 +0200

    initial project

---
+++ src/gifts/tests/basic_test.py
@@ -0,0 +1,13 @@
+'''
+gaia: Test module.
+
+Meant for use with py.test.
+Write each test as a function named test_<something>.
+Read more here: http://pytest.org/
+
+Copyright 2016, Thomas Latzko
+Licensed under MIT
+'''
+
+def test_example():
+    assert True
